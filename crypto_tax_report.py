932680cbe55badf56a9a57d376a1739ab5810ccc
Andreas <andreas.knittel@gmx.de>
2023-09-06 12:35:01 -0400

    Added functionality to add and remove transactions.

--- crypto_tax_report.py
+++ crypto_tax_report.py
@@ -2,6 +2,7 @@
 
 import csv
 import datetime
+import functools
 import re
 from enum import Enum
 
@@ -20,6 +21,21 @@ class Currency(Enum):
     MANA = 11
     ELON = 12
     EUR = 13
+
+class Heading(Enum):
+    TIMESTAMP = 0
+    IDENTIFIER = 1
+    CURRENCY = 2
+    CURRENCY_AMOUNT = 3
+    TARGET_CURRENCY = 4
+    TARGET_AMOUNT = 5
+    SOURCE_CURRENCY = 6
+    SOURCE_AMOUNT = 7
+    NATIVE_CURRENCY = 8
+    NATIVE_CURRENCY_AMOUNT = 9
+    DOLLAR_AMOUNT = 10
+    INTERNAL_IDENTIFIER = 11
+    HASH_KEY = 12
 
 # Define the regex pattern with named groups
 currencyExchangePattern = r'\s*(?P<FromCurrency>[\w]+)\s*->\s*(?P<ToCurrency>[\w]+)\s*'
@@ -48,13 +64,13 @@ def matchCurrencyExchangePattern(stringToMatch):
         toCurrency = match.group('ToCurrency')
     return (isAMatch, fromCurrency, toCurrency)
 
-def buyCryptoCurrencyWithEuro(stringToMatch):
+def matchBuyCryptoCurrencyWithEuro(stringToMatch):
     isAMatch, fromCurrency, toCurrency = matchCurrencyExchangePattern(stringToMatch)
     if isAMatch and fromCurrency==Currency.EUR.name:
         return (True, toCurrency)
     return (False, '')
 
-def sellCryptoCurrencyGetEuro(stringToMatch):
+def matchSellCryptoCurrencyGetEuro(stringToMatch):
     isAMatch, fromCurrency, toCurrency = matchCurrencyExchangePattern(stringToMatch)
     if isAMatch and toCurrency==Currency.EUR.name:
         return (True, fromCurrency)
@@ -65,9 +81,29 @@ class CurrencyEntry:
     def __init__(self, dateTime, amount, actualValueEuro):
         self.dateTime = dateTime
         self.amount = amount
-        self.valueInEuro = actualValueEuro
+        self.boughtAt = actualValueEuro
 
-class CurrencyData:
+
+class TransactionRemover:
+    def __init__(self, initialCryptoAmount):
+        self.cryptoAmountToBeRemoved = initialCryptoAmount
+        self.removedCryptoBoughtAt = 0.0
+        self.newCryptoTransactions = []
+
+    def __call__(self, currencyEntry):
+        if self.cryptoAmountToBeRemoved > currencyEntry.amount:
+            self.cryptoAmountToBeRemoved -= currencyEntry.amount
+            self.removedCryptoBoughtAt += currencyEntry.boughtAt
+        elif self.cryptoAmountToBeRemoved > 0.0:
+            relativeReductionOfEntry = ( currencyEntry.amount - self.cryptoAmountToBeRemoved ) / currencyEntry.amount
+            self.removedCryptoBoughtAt += (1.0 - relativeReductionofEntry) * currencyEntry.boughtAt
+            self.newCryptoTransactions.append(CurrencyEntry(currencyEntry.dateTime, currencyEntry.amount - self.cryptoAmountToBeRemoved, currencyEntry.boughtAt * relativeReductionOfEntry))
+            self.cryptoAmountToBeRemoved = 0.0
+        else:
+            self.newCryptoTransactions.append(currencyEntry)
+
+
+class TransactionData:
     def __init__(self):
         self.dataSet = {}
 
@@ -75,6 +111,21 @@ class CurrencyData:
         if not cryptoCurrency in dataSet:
             self.dataSet[cryptoCurrency] = []
         self.dataSet[cryptoCurrency].append(currencyEntry)
+
+    def remove(self, cryptoCurrency, amount):
+        if not cryptoCurrency in dataSet:
+            print("Logical error: there should be an entry for the crypto currency {cryptoCurrency}.")
+            return 0.0
+        transactionRemover = TransactionRemover(amount)
+        for currencyEntry in dataSet[cryptoCurrency]:
+            transactionRemover(currencyEntry)
+        dataSet[cryptoCurrency] = transactionRemover.newCryptoTransactions
+        return transactionRemover.removedCryptoBoughtAt
+
+    def swap(self, sourceCurrency, sourceAmount, targetCurrency, targetAmount, transactionDateTime):
+        boughtAt = self.remove(sourceCurrency, sourceAmount)
+        self.add(targetCurrency, CurrencyEntry(transactionDateTime, targetAmount, boughtAt))
+
 
 def main():
     transactionList = []
